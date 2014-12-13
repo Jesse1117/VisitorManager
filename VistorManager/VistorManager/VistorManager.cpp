ca3ef8a487ab11975bfb0c60ffedf5485af9ff68
hlj <hlj1990515@gmail.com>
2014-12-13 10:54:30 +0800

    修改 boost

---
+++ VistorManager/VistorManager/VistorManager.cpp
@@ -0,0 +1,20 @@
+// VistorManager.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "VistorManager.h"
+#include "MainFrame.h"
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+//	CPaintManagerUI::SetResourcePath(_T("xmls"));
+	CMainFrame *pFrame = new CMainFrame(_T("xmls\\MainFrame.xml"));
+	pFrame->Create(NULL, _T("MainFrameWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	pFrame->ShowModal();
+	delete pFrame;
+	return 0;
+}
+
